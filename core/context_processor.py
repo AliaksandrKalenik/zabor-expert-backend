7fbca599cd4abe44668000dce5f680dd3537464f
Aliaksandr Kalenik <a.kalenik94@gmail.com>
2018-03-26 16:39:50 +0300

    disabled product

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,10 +1,11 @@
-from product import models as mx
+# from product import models as mx
 
 
 def requisites(request):
-    return {
-        "requisites": {
-            k: v for k, v in mx.Requisites.objects.first().__dict__.items()
-            if k in [f.name for f in mx.Requisites._meta.local_fields]
-        }
-    }
+    return {}
+    # return {
+    #     "requisites": {
+    #         k: v for k, v in mx.Requisites.objects.first().__dict__.items()
+    #         if k in [f.name for f in mx.Requisites._meta.local_fields]
+    #     }
+    # }
